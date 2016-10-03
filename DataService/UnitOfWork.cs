d6a9bc7901e30b8a05a3053f7cee80267ae178e3
huynhngoc <huynhbaongoc_0412@yahoo.com.sg>
2016-10-03 16:03:44 +0700

    Model, Repo, Service

---
+++ DataService/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService
+{
+    public interface IUnitOfWork
+    {
+        void Save();
+        Task SaveAsync();
+    }
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private EFPEntities entities { get; set; }
+
+        public UnitOfWork(EFPEntities entities)
+        {
+            this.entities = entities;
+        }
+
+        public void Save()
+        {
+            entities.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await this.entities.SaveChangesAsync();
+        }
+    }
+}
