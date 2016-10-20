69e62541967c687e3eeda9bf770f77d937203188
long189b <long189b@gmail.com>
2016-10-20 11:45:09 +0700

    Fix Aftermerge
    add CategoryParentVM

---
+++ DataService/ViewModel/CategoryParentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.ViewModel
+{
+    public class CategoryParentViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ShopId { get; set; }
+        public List<CategoryParentViewModel> Children { get; set; }
+    }
+}
