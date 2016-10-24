e7be9754450c37587842949824886d01e68a187c
huynhngoc <huynhbaongoc_0412@yahoo.com.sg>
2016-10-24 14:34:33 +0700

    Add comment + add conversation

--- DataService/Utils/WebhookObject.cs
+++ DataService/Utils/WebhookObject.cs
@@ -6,9 +6,33 @@ using System.Threading.Tasks;
 
 namespace DataService.Utils
 {
-    class WebhookObject
+    public static class WebhookObject
     {
+        public const string User = "user";
+        public const string Page = "page";
+        public const string Permission = "permission";
+        public const string Payment = "payments";
     }
 
+    public static class WebhookField
+    {
+        public const string Feed = "feed";
+        public const string Conversations = "conversations";
+    }
+
+    public static class WebhookItem
+    {
+        public const string Comment = "comment";
+        public const string Reaction = "reaction";
+        public const string Like = "like";
+    }
+
+    public static class WebhookVerb
+    {
+        public const string Add = "add";
+        public const string Edit = "edited";
+        public const string Remove = "remove";
+        public const string Hide = "hide";
+    }
     
 }
--- ShopManager/Controllers/WebhookController.cs
+++ ShopManager/Controllers/WebhookController.cs
@@ -5,11 +5,17 @@ using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
+using DataService.Utils;
+using System.Dynamic;
+using DataService.Service;
+using Facebook;
 
 namespace ShopManager.Controllers
 {
     public class WebhookController : Controller
     {
+        ShopService shopService = new ShopService();
+        CommentService commentService = new CommentService();
         // GET: Webhook
         public ActionResult Index()
         {
@@ -33,6 +39,7 @@ namespace ShopManager.Controllers
             System.Diagnostics.Debug.WriteLine(jsonData);
             
             dynamic fbJson = System.Web.Helpers.Json.Decode(jsonData);
+            ProcessItem(fbJson);
 
             System.Diagnostics.Debug.WriteLine("header");
             System.Diagnostics.Debug.WriteLine(this.Request.Headers["X-Hub-Signature"]);
@@ -49,16 +56,87 @@ namespace ShopManager.Controllers
             foreach (dynamic entry in entries)
             {
                 string shopId = entry.id;
-                string time = entry.time;
+                long time = entry.time;
+
                 dynamic changes = entry.changes;
-                foreach(dynamic change in changes)
+                foreach (dynamic change in changes)
                 {
                     string field = change.field;
                     dynamic value = change.value;
+                    if (field.Equals(WebhookField.Feed))
+                    {
+                        string commentId = value.comment_id;
+                        string item = value.item;
+                        if (item.Equals(WebhookItem.Comment))
+                        {
+                            string verb = value.verb;                            
+                            long createTime = value.time;
+                            string customerId = value.sender_id;
+                            int intent = 1;
+                            switch (verb)
+                            {
+                                case WebhookVerb.Add:                                                                                                                                                
+                                case WebhookVerb.Edit:
+                                    if (HasProperty(value, "message"))
+                                    {
+                                        string message = value.message;
+                                        //chatbot api for message here
+                                    }
+                                    if (HasProperty(value, "photo"))
+                                    {
+                                        //chatbot api intent photo here
+
+                                    }
+                                    if (CheckTagOthers(commentId, shopId))
+                                    {
+                                        //chatbot api intent tag other here
+                                    }
+                                    commentService.AddComment(commentId, customerId, createTime, intent, shopId);
+                                    break;
+                                case WebhookVerb.Hide:
+
+                                    break;
+                                case WebhookVerb.Remove: break;
+                                default: break;
+                            }
+                            
+                        }
+                    } else if (field.Equals(WebhookField.Conversations)){
+
+                    }                    
+
                 }
             }
 
 
+        }
+
+        private bool CheckTagOthers(string commentId, string shopId)
+        {
+            string accessToken = shopService.GetShop(shopId).FbToken;
+            FacebookClient fb = new FacebookClient(accessToken);
+            dynamic param = new ExpandoObject();
+            param.fields = "message_tags";
+            dynamic result = fb.Get(commentId, param);
+            if (HasProperty(result, "message_tags"))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        private bool HasProperty(dynamic obj, string name)
+        {
+            Type objType = obj.GetType();
+
+            if (objType == typeof(ExpandoObject))
+            {
+                return ((IDictionary<string, object>)obj).ContainsKey(name);
+            }
+
+            return objType.GetProperty(name) != null;
         }
     }
 }
