71411a7c155442a716ed137dfff5bbc74ee4137c
huynhngoc <huynhbaongoc_0412@yahoo.com.sg>
2016-10-27 23:21:21 +0700

    fixed

--- DataService/Comment.cs
+++ DataService/Comment.cs
@@ -14,15 +14,23 @@ namespace DataService
     
     public partial class Comment
     {
+        public Comment()
+        {
+            this.Comments1 = new HashSet<Comment>();
+        }
+    
         public string Id { get; set; }
-        public string FbUserId { get; set; }
-        public string ShopId { get; set; }
+        public string SenderFbId { get; set; }
+        public string PostId { get; set; }
         public bool IsRead { get; set; }
         public int Status { get; set; }
         public int IntentId { get; set; }
         public System.DateTime DateCreated { get; set; }
+        public string ParentId { get; set; }
     
+        public virtual ICollection<Comment> Comments1 { get; set; }
+        public virtual Comment Comment1 { get; set; }
         public virtual Intent Intent { get; set; }
-        public virtual Shop Shop { get; set; }
+        public virtual Post Post { get; set; }
     }
 }
