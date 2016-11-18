bbebcd0ae8d03663f97ff1f73857377e67401723
KonniSekai <red02498@yahoo.com>
2016-11-18 17:15:18 +0700

    Add ResponseRepo

---
+++ DataService/Repository/ResponseRepository.cs
@@ -0,0 +1,28 @@
+﻿using DataService.JqueryDataTable;
+using DataService.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.Repository
+{
+    public class ResponseRepository : BaseRepository<Respons>
+    {
+        public ResponseRepository() : base()
+        {
+
+        }
+
+        public List<Respons> FindByShopAndIntent(string shopId, int intentId)
+        {
+            return dbSet.Where(q => q.ShopId == shopId && q.IntentId == intentId).ToList();
+        }
+
+        public List<Respons> GetAll(string shopId)
+        {
+            return dbSet.Where(q => q.ShopId == shopId).ToList();
+        }
+    }
+}
