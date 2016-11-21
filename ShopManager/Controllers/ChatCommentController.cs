ebeaace79045c0c6644e75aa6c70395237acc4df
long189b <long189b@gmail.com>
2016-11-21 08:01:29 +0700

    Fix CCController

--- ShopManager/Controllers/ChatCommentController.cs
+++ ShopManager/Controllers/ChatCommentController.cs
@@ -1057,59 +1057,6 @@ namespace ShopManager.Controllers
             }
         }
 
-        public JsonResult ReplyComment(string commentId, string message)
-        {
-            string accessToken = (shopService.GetShop((string)Session["ShopId"])).FbToken;
-            dynamic param = new ExpandoObject();
-            try
-            {
-                param.access_token = accessToken;
-                param.message = message;
-                var result = fbApp.Post(commentId + "/comments", param);
-                return Json(result, JsonRequestBehavior.AllowGet);
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine(e.StackTrace);
-                return Json(new { success = false, e.Message }, JsonRequestBehavior.AllowGet);
-            }
-        }
-
-        public JsonResult HideComment(string commentId)
-        {
-            string accessToken = (shopService.GetShop((string)Session["ShopId"])).FbToken;
-            dynamic param = new ExpandoObject();
-            try
-            {
-                param.access_token = accessToken;
-                param.is_hidden = true;
-                var result = fbApp.Post(commentId, param);
-                return Json(result, JsonRequestBehavior.AllowGet);
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine(e.StackTrace);
-                return Json(new { success = false, e.Message }, JsonRequestBehavior.AllowGet);
-            }
-        }
-
-        public JsonResult DeleteComment(string commentId)
-        {
-            string accessToken = (shopService.GetShop((string)Session["ShopId"])).FbToken;
-            fbApp.AccessToken = accessToken;
-            dynamic param = new ExpandoObject();
-            try
-            {
-                var result = fbApp.Delete(commentId);
-                return Json(result, JsonRequestBehavior.AllowGet);
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine(e.StackTrace);
-                return Json(new { success = false, e.Message }, JsonRequestBehavior.AllowGet);
-            }
-        }
-
         public JsonResult PrivateReplyComment(string commentId, string message)
         {
             string accessToken = (shopService.GetShop((string)Session["ShopId"])).FbToken;
